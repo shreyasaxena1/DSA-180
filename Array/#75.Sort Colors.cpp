a8b3646adba0fe994ec13bf0e02e4f78666a4f4b
Shreya Saxena <shreyasaxena117@gmail.com>
2021-05-20 13:27:48 +0530

    Update #75.Sort Colors.cpp

--- Array/#75.Sort Colors.cpp
+++ Array/#75.Sort Colors.cpp
@@ -14,7 +14,7 @@ public:
         int n = nums.size();
         int low = 0, mid = 0, high = n - 1;
 
-        for (int i = 0; i < n; i++)
+        while(mid<=high)
         {
             switch (nums[mid])
             {
