1c8730106963801a04284fe87edd602bb033e05c
Shreya Saxena <shreyasaxena117@gmail.com>
2021-06-01 23:58:39 +0530

    Add files via upload

---
+++ Two Pointers/#26. Remove Duplicates from Sorted Array.cpp
@@ -0,0 +1,19 @@
+class Solution
+{
+public:
+    int removeDuplicates(vector<int> &nums)
+    {
+        int i = 0, n = nums.size();
+        if (n == 0)
+            return 0;
+        for (int j = 1; j < n; j++)
+        {
+            if (nums[i] != nums[j])
+            {
+                i++;
+                nums[i] = nums[j];
+            }
+        }
+        return i + 1;
+    }
+};
