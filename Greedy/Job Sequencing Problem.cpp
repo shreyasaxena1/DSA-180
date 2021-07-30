078c4e9c7a0c542ff7e9bce0c087a4b902e6a3ba
Shreya Saxena <shreyasaxena117@gmail.com>
2021-07-30 23:44:40 +0530

    Update Job Sequencing Problem.cpp

--- Greedy/Job Sequencing Problem.cpp
+++ Greedy/Job Sequencing Problem.cpp
@@ -1,33 +1,35 @@
-bool comparison(Job a, Job b) {
-     return (a.profit > b.profit);
-}
+class Solution {
+public:
+  // Function to find the maximum profit and the number of jobs done.
+  static bool comparison(Job a, Job b) { return (a.profit > b.profit); }
+  vector<int> JobScheduling(Job arr[], int n) {
 
+    vector<int> ans(2);
+    sort(arr, arr + n, comparison);
+    int maxi = arr[0].dead;
+    for (int i = 1; i < n; i++) {
+      maxi = max(maxi, arr[i].dead);
+    }
 
-pair<int, int> JobScheduling(Job arr[], int n) {
+    int slot[maxi + 1];
 
-  sort(arr, arr + n, comparison);
-  int maxi = arr[0].dead;
-  for (int i = 1; i < n; i++) {
-    maxi = max(maxi, arr[i].dead);
-  }
+    for (int i = 0; i <= maxi; i++)
+      slot[i] = -1;
 
-  int slot[maxi + 1];
+    int countJobs = 0, jobProfit = 0;
 
-  for (int i = 0; i <= maxi; i++)
-    slot[i] = -1;
-
-  int countJobs = 0, jobProfit = 0;
-
-  for (int i = 0; i < n; i++) {
-    for (int j = arr[i].dead; j > 0; j--) {
-      if (slot[j] == -1) {
-        slot[j] = arr[i].id;
-        countJobs++;
-        jobProfit += arr[i].profit;
-        break;
+    for (int i = 0; i < n; i++) {
+      for (int j = arr[i].dead; j > 0; j--) {
+        if (slot[j] == -1) {
+          slot[j] = arr[i].id;
+          countJobs++;
+          jobProfit += arr[i].profit;
+          break;
+        }
       }
     }
-  }
 
-  return make_pair(countJobs, jobProfit);
-}
+    ans[0] = countJobs, ans[1] = jobProfit;
+    return ans;
+  }
+};
