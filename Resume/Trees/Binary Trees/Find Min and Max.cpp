f3202dc33d9099f6eba2eee49c880959b67c6861
Shreya Saxena <shreyasaxena117@gmail.com>
2021-11-21 20:24:48 +0530

    Create Find Min and Max.cpp

---
+++ Resume/Trees/Binary Trees/Find Min and Max.cpp
@@ -0,0 +1,18 @@
+class Solution
+{
+public:
+    int findMax(Node *root)
+    {
+        if(root==NULL)
+          return INT_MIN;
+        
+        return max(root->data,max(findMax(root->left),findMax(root->right)));
+    }
+    int findMin(Node *root)
+    {
+        if(root==NULL)
+           return INT_MAX;
+        
+        return min(root->data,min(findMin(root->left),findMin(root->right)));
+    }
+};
