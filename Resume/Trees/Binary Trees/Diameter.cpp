e1301b0bc0d94731bdfe3323c99310e854612819
Shreya Saxena <shreyasaxena117@gmail.com>
2021-11-24 23:51:34 +0530

    DA

---
+++ Resume/Trees/Binary Trees/Diameter.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int diameter=0;
+    int diameterOfBinaryTree(TreeNode* root) {
+      height(root);
+      return diameter-1;
+    }
+    
+    int height(TreeNode *root){
+        if(root==NULL)
+            return 0;
+        
+        int lh = height(root->left);
+        int rh = height(root->right);
+        
+        if(diameter<1+lh+rh){
+            diameter = 1+lh+rh;
+        }
+        
+        return 1+ (lh>rh ? lh:rh);
+    }
+};
