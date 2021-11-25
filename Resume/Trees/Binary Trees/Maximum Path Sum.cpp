78cf79faa46537458538202d7f778490faf0c9f8
Shreya Saxena <shreyasaxena117@gmail.com>
2021-11-25 21:48:02 +0530

    Update Maximum Path Sum.cpp

--- Resume/Trees/Binary Trees/Maximum Path Sum.cpp
+++ Resume/Trees/Binary Trees/Maximum Path Sum.cpp
@@ -1,1 +1,25 @@
-
+class Solution {
+  public:
+    //Function to return maximum path sum from any node in a tree.
+    
+    int findMaxSumUtil(Node* root, int &maxi){
+        if(!root) return 0;
+       
+       int leftSum = max(0,findMaxSumUtil(root->left,maxi));
+       int rightSum = max(0,findMaxSumUtil(root->right,maxi));
+       
+       maxi=max(maxi,root->data+leftSum+rightSum);
+      
+       return root->data+max(leftSum,rightSum);
+    }
+    
+    int findMaxSum(Node* root)
+    {
+       
+       int maxi=INT_MIN;
+       
+       findMaxSumUtil(root,maxi);
+       
+       return maxi;
+    }
+};
