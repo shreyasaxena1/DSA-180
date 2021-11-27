617503ece7e0e07b539b3b3787bcf1eabeca993d
Shreya Saxena <shreyasaxena117@gmail.com>
2021-11-27 12:02:34 +0530

    Create Flatten BT.cpp

---
+++ Resume/Trees/Binary Trees/Flatten BT.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    void flatten(TreeNode* root) {
+        if(root == NULL) return;
+        TreeNode* cur = root;
+		while (cur)
+		{
+			if(cur->left)
+			{
+				TreeNode* pre = cur->left;
+				while(pre->right)
+				{
+					pre = pre->right;
+				}
+				pre->right = cur->right;
+				cur->right = cur->left;
+				cur->left = NULL;
+			}
+			cur = cur->right;
+		}
+    }
+};
